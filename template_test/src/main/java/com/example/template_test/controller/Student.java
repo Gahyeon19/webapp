05dc50cef8338ae3b19efa2591dda00046aeb99c
Gahyeon19 <wjsrkgus0119@naver.com>
2025-01-14 12:49:23 +0900

    practice Thymeleaf

---
+++ template_test/src/main/java/com/example/template_test/controller/Student.java
@@ -0,0 +1,12 @@
+package com.example.template_test.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+
+@Getter
+@Setter
+public class Student {
+    private String name;
+    private int score;
+}
